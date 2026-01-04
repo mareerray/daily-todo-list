a71abd7c83671c08ef31f79f83614e8a0c18e762
mareerray <mareerray@gmail.com>
2026-01-04 14:43:35 +0200

    Refactor service worker install event to filter cache URLs and handle optional icons

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('todo-v1').then((cache) => cache.addAll([
-      '/', '/index.html', '/style.css', '/app.js', '/icon-192.png', '/icon-512.png'
-    ])),
+    caches.open('todo-v1').then((cache) => 
+  cache.addAll([
+    '/', '/index.html', '/style.css', '/app.js'
+    // Icons optional
+  ].filter(url => url)) // Safe
+).catch(err => console.log('Cache optional', err))
+
+    // caches.open('todo-v1').then((cache) => cache.addAll([
+    //   '/', '/index.html', '/style.css', '/app.js', '/icon-192.png', '/icon-512.png'
+    // ])),
   );
 });
 
