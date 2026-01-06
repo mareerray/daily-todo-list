ff48950346a9f6fd619bc7f4dafccd1fa6065c7f
mareerray <mareerray@gmail.com>
2026-01-06 23:44:27 +0200

    Update service worker cache version and add missing icons to cache

--- sw.js
+++ sw.js
@@ -1,11 +1,19 @@
-const CACHE_NAME = 'todo-v5';
+const CACHE_NAME = 'todo-v6';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => 
       cache.addAll([
-        '/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/favicon.ico', '/translations.json'
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/manifest.json',
+        '/favicon.ico',
+        '/translations.json',
+        '/icon-192.png',
+        '/icon-512.png'
       ].filter(url => url))
     ).catch(err => console.log('Cache optional', err))
   );
@@ -21,6 +29,8 @@ self.addEventListener('activate', (e) => {
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request)
+      .then((cached) => cached || fetch(e.request))
+      .catch(() => new Response('', { status: 503, statusText: 'Offline' }))
   );
 });
